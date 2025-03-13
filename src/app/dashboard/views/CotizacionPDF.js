36c7f2ee568f2387cb1941e6aeac2214a74a39bc
IvanMarte <ivanbiasfx@gmail.com>
2025-03-13 17:02:25 -0600

    fix: descuentos

--- src/app/dashboard/views/CotizacionPDF.js
+++ src/app/dashboard/views/CotizacionPDF.js
@@ -108,7 +108,7 @@ const CotizacionPDF = ({ order }) => {
   ? parseFloat(order.discount.cantidad_dinero)
   : 0;
 
-  const grandTotal = total + discountAmount;
+  const grandTotal = total - discountAmount;
 
   return (
     <Document>
